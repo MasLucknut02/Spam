b715036be0ad8b6babe9392b90fafb2cae3ff126
MasLucknut02 <108270971+MasLucknut02@users.noreply.github.com>
2022-06-29 10:21:10 +0700

    Update asiapspam.py

--- asiapspam.py
+++ asiapspam.py
@@ -10,8 +10,8 @@ print("[-] Wa        : +6281384307028")
 print("[+] Github    : https://github.com/MasLucknut02")
 print(" Have fun aja banh")
 print("Jangan dibuat serius")
-nomor = input("\n[?] input Nomor : ")
-jumlah = input("[?] input berapa banh : ")
+nomor = input("\n[?] Nomornya banh : ")
+jumlah = input("[?] berapa banh : ")
 print()
 data = {
 "nomor": nomor
@@ -19,6 +19,6 @@ data = {
 for MasLucknut_2 in range(int(jumlah)):
         sanz = requests.get(url_api_spam_wa_mantan, params=data)
         if "Berhasil Ngab ! .. Subrek Yt FREE TUTORIAL." in sanz.text:
-                 print("[+] Berhasil Ya Banh")
+                 print("[+] Ok,Berhasil Ya Banh")
         else:
                  print("[!] Waduh kok gagal banh")
